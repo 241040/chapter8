a3e2e2effd5e0b73e816d5ff953624189287370a
241040 <241040@std.hi-joho.ac.jp>
2025-06-09 11:29:06 +0900

    6/9

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+
+        Sword s = new Sword();
+        s.name = "勇者の剣(レプリカ)";
+        s.damage = 10;
+
         Hero h = new Hero();
         h.name = "勇者ヒンメル";
         h.hp = 100;
+        h.sword = s;
+
+        System.out.println("現在の武器は" + h.sword.name);
 
         Matango m1 = new Matango();
         m1.hp = 50;
